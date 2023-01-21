a9b12c27bae304e2ddc2ac433da9950ffe776366
RDiGregorio <robertdigreg@gmail.com>
2023-01-21 16:48:13 -0500

    hello world

---
+++ src/lib/uuid.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+let count = 0;
+
+/**
+ * Returns a universally unique identifier.
+ * @returns {string}
+ */
+
+export function uuid() {
+  return [_.random(0, Number.MAX_SAFE_INTEGER), Date.now(), count++].map(number => number.toString(16)).join('-');
+}
