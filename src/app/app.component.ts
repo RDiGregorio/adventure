76867fcb21a016ef048caec6f1535829e34cf3a9
RDiGregorio <robertdigreg@gmail.com>
2023-01-21 17:11:22 -0500

    add uuid function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {uuid} from '../lib/uuid';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,6 @@ export class AppComponent {
   greeting = '';
 
   constructor() {
-    this.greeting = 'hello world';
+    this.greeting = uuid();
   }
 }
