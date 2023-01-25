fd5bfb982df77f5ce5ed9a7fdecd80aea9a473a3
RDiGregorio <robertdigreg@gmail.com>
2023-01-25 10:12:48 -0500

    experiments

---
+++ src/lib/util/space.js
@@ -0,0 +1,41 @@
+import RTree from 'rtree';
+
+class Space {
+    #rTree = new RTree();
+    #map = new Map();
+
+    /**
+     * @param {*} value
+     * @param {number} x
+     * @param {number} y
+     */
+
+    add(value, x, y) {
+        this.delete(value);
+        this.#map.set(value, [x, y]);
+        this.#rTree.insert({x: x, y: y, w: 0, h: 0}, value);
+    }
+
+    /**
+     * @param {*} value
+     */
+
+    delete(value) {
+        if (!this.#map.has(value)) return;
+        const [x, y] = this.#map.get(value);
+        this.#map.delete(value);
+        this.#rTree.remove({x: x, y: y, w: 0, h: 0}, value);
+    }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @param {number} width
+     * @param {number} height
+     * @return {*[]}
+     */
+
+    search(x, y, width, height) {
+        return this.#rTree.bbox([x, y], [x + width - 1, y + height - 1]);
+    }
+}
