3cb5fc2d0f2902be9d7eed9dd920e86737bd45ed
RDiGregorio <robertdigreg@gmail.com>
2023-01-25 18:34:06 -0500

    experiments

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import './app.css';
+
+export default function App() {
+    return (
+        <div className="app">
+            <p>
+                Hello world!
+            </p>
+        </div>
+    );
+}
