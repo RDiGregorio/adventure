f1c4ad78f67b2cc4004183815fc0b870f96a87a2
RDiGregorio <robertdigreg@gmail.com>
2023-01-27 22:14:20 -0500

    experiments

--- server/main.js
+++ server/main.js
@@ -5,7 +5,8 @@ import {Entity} from 'shared/src/game/entity.js';
 
 const manager = new EntityManager(new Storage(mockExists, mockLoad, mockSave, jsonReviver, jsonReplacer));
 const entity = new Entity();
-manager.add(entity, 0, 0, 0);
+manager.add(entity, 0, 100, 200);
+manager.add(new Entity(), 0, -1000, 500);
 console.log(manager.toMap());
 manager.delete(entity);
 console.log(manager.toMap());
