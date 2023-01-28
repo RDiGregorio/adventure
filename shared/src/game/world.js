bee67a813f890a8ec452c00a2bfc228ae0f7da17
RDiGregorio <robertdigreg@gmail.com>
2023-01-27 21:55:52 -0500

    experiments

--- shared/src/game/world.js
+++ shared/src/game/world.js
@@ -19,7 +19,7 @@ export class EntityManager {
     }
 
     #key(world, x, y, width, height) {
-        return `${world} ${x} ${y} ${width} ${height}`;
+        return `world/${world} ${x} ${y} ${width} ${height}`;
     }
 
     /**
@@ -31,7 +31,7 @@ export class EntityManager {
      * @return {Entity[]}
      */
 
-    static search(world, x, y, width, height) {
+    search(world, x, y, width, height) {
         return this.#spaces.get(world)?.search(x, y, width, height) ?? [];
     }
 
@@ -84,114 +84,9 @@ export class EntityManager {
      * @return {Promise<void>}
      */
 
-    save(world, x, y, width, height, unload = false) {
-
-    }
-}
-
-export class World2 {
-    static #size = 100;
-    static storage = new Storage();
-    static #worlds = new Map();
-    #loaded = new Set();
-    #space = new Space(entity => entity.id);
-    #key;
-
-    /**
-     * @return {Iterable<[number,number]>}
-     */
-
-    get points() {
-        return this.#space.points;
-    }
-
-    /**
-     * @param {string} key
-     * @return {World}
-     */
-
-    static get(key) {
-        if (!World.#worlds.has(key)) {
-            const world = new World();
-            World.#worlds.set(world.#key = key, world);
-        }
-
-        return World.#worlds.get(key);
-    }
-
-    #storageKey(x, y) {
-        return `world/${this.#key} ${x} ${y}`;
-    }
-
-    /**
-     * @param {Entity} entity
-     */
-
-    add(entity) {
-        this.#space.add(entity, entity.x, entity.y);
-    }
-
-    /**
-     * @param {Entity} entity
-     */
-
-    delete(entity) {
-        this.#space.delete(entity);
-    }
-
-    /**
-     * @param {number} x
-     * @param {number} y
-     * @return {Promise<void>}
-     */
-
-    async load(x, y) {
-        const key = this.#storageKey(x, y);
-
-        // todo: this is actually super buggy, and i'll need a queue here, too
-
-        if (this.#loaded.add(key) && await World.storage.exists(key)) for (const entity of await World.storage.load(key)) this.#space.add(entity, entity.x, entity.y);
-    }
-
-    /**
-     * @param {number} x
-     * @param {number} y
-     * @param {number} width
-     * @param {number} height
-     * @return {Entity[]}
-     */
-
-    search(x, y, width, height) {
-        return this.#space.search(x, y, width, height);
-    }
-
-    /**
-     * @param {number} x
-     * @param {number} y
-     * @return {Promise<void>}
-     */
-
-    async save(x, y) {
-        // todo: probably shouldn't save players...
-
-        const key = this.#storageKey(x, y);
-        await World.storage.save(key, this.search(x, y, World.#size, World.#size));
-    }
-
-    /**
-     * @param {number} x
-     * @param {number} y
-     * @return {Promise<void>}
-     */
-
-    async unload(x, y) {
-        // todo: should i use another queue here?
-
-        const key = this.#storageKey(x, y);
-        if (!this.#loaded.has(key)) return;
-        await this.save(x, y);
-
-        // if (!this.#loaded.delete(key)) return;
-        this.#space.search(x, y, World.#size, World.#size).forEach(this.delete);
+    async save(world, x, y, width, height, unload = false) {
+        const key = this.#key(world, x, y, width, height), entities = this.search(world, x, y, width, height);
+        await this.#storage.save(key, entities);
+        if (unload) entities.forEach(this.delete);
     }
 }
