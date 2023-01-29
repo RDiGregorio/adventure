43a7f8f4af03ab47499fe3ccae8c4bc87a10d9e3
RDiGregorio <robertdigreg@gmail.com>
2023-01-29 16:56:24 -0500

    experiments

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,25 @@
+import {sleep} from 'shared/src/util/async';
 import './app.css';
+import React from "react";
 
-export default function App() {
-    return (
-        <div className="noto-emoji">
-            <p>
-                🥰💀✌️🌴🐢🐐🍄⚽🍻👑📸😬👀🚨🏡🕊️🏆😻🌟🧿🍀🎨🍜
-            </p>
-        </div>
-    );
+export default class App extends React.Component {
+    render() {
+        const rows = [];
+
+        function f() {
+            // await sleep(1000);
+            for (let y = 0; y < 10; y++) {
+                const row = [];
+
+                for (let x = 0; x < 10; x++)
+                    row.push(<span>💀1</span>);
+
+                rows.push(<div>{row}</div>);
+            }
+        }
+
+        f();
+
+        return (<div className="noto-emoji">{rows}</div>);
+    }
 }
