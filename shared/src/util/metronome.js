72cd836ba51852a9ce90e6aa3d2e507e79923c42
RDiGregorio <robertdigreg@gmail.com>
2023-01-29 13:45:57 -0500

    experiments

---
+++ shared/src/util/metronome.js
@@ -0,0 +1,22 @@
+import {sleep} from "./async.js";
+
+export class Metronome {
+    #running = true;
+
+    constructor(interval, callback) {
+        this.#run(interval, callback);
+    }
+
+    async #run(interval, callback) {
+        const start = Date.now();
+
+        for (let offset = 0; this.#running; offset += interval) {
+            await sleep(Math.max(0, start - Date.now() + offset));
+            callback();
+        }
+    }
+
+    stop() {
+        this.#running = false;
+    }
+}
