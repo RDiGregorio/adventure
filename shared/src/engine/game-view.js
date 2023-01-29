f38dabc87899997e3d7cd8793bb32a003dde435c
RDiGregorio <robertdigreg@gmail.com>
2023-01-28 23:44:24 -0500

    experiments

--- shared/src/engine/game-view.js
+++ shared/src/engine/game-view.js
@@ -1,3 +1,6 @@
 class GameView {
-
+    #replaceWith(left, right) {
+        [...left.keys()].filter(key => !right.has(key)).forEach(key => left.delete(key));
+        [...right].forEach(entry => left.set(...entry));
+    }
 }
