96eb2ee8114af8be006ab6f8c95d56a272da8528
RDiGregorio <robertdigreg@gmail.com>
2023-01-29 19:22:44 -0500

    experiments

--- client/src/app.js
+++ client/src/app.js
@@ -1,35 +1,45 @@
-import {sleep} from 'shared/src/util/async';
+import _ from 'lodash';
 import './app.css';
-import React from "react";
+import React from 'react';
+import {GameView} from 'shared/src/engine/game-view';
+import {Entity} from 'shared/src/engine/entity';
+import {ChunkManager} from "shared/src/engine/chunk-manager";
+import {mockExists, mockLoad, mockSave, Storage} from 'shared/src/util/storage';
+import {jsonReplacer, jsonReviver, registerJsonReplacer} from 'shared/src/util/json';
+import {random} from 'shared/src/util/math';
+import {GameObject} from 'shared/src/engine/game-object';
 
 export default class App extends React.Component {
     constructor(properties) {
         super(properties);
-        this.state = {date: Date.now()};
-        setInterval(() => this.setState({date: Date.now()}), 100);
+        this.state = {data: ''};
+        this.#init();
+    }
+
+    async #init() {
+        const chunkManager = new ChunkManager(
+            100,
+            new Storage(mockExists, mockLoad, mockSave, jsonReviver, jsonReplacer),
+            () => _.range(5).map(() => new Entity(0, random(100), random(100)))
+        );
+
+        const entity = new Entity(0, 0, 0);
+        const gameView = new GameView(entity, chunkManager);
+        await chunkManager.load(0, 0, 0);
+        await gameView.load();
+        registerJsonReplacer(GameObject.jsonReplacer);
+        const data = JSON.stringify(gameView, jsonReplacer);
+        this.setState({data: data});
+    }
+
+    #tiles(width, height) {
+        return _.range(height).map(y => <div>{_.range(width).map(x => {
+            const id = `tile-${x}-${y}`;
+            return <span id={id}>[{x}, {y}]</span>;
+        })}</div>);
     }
 
     render() {
-        return <div>{this.state.date}</div>
-
-        /*
-                const rows = [];
-
-                function f() {
-                    // await sleep(1000);
-                    for (let y = 0; y < 10; y++) {
-                        const row = [];
-
-                        for (let x = 0; x < 10; x++)
-                            row.push(<span>💀1</span>);
-
-                        rows.push(<div>{row}</div>);
-                    }
-                }
-
-                f();
-
-                return (<div className="noto-emoji">{rows}</div>);
-        */
+        return <div className="noto-emoji">{this.#tiles(10, 10)}</div>
     }
 }
