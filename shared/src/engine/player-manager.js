a33e98800a57c64afe37b395d473a978e1d8a806
RDiGregorio <robertdigreg@gmail.com>
2023-01-30 16:28:39 -0500

    experiments

--- shared/src/engine/player-manager.js
+++ shared/src/engine/player-manager.js
@@ -1,2 +1,27 @@
 class PlayerManager {
+    #storage;
+    #create;
+
+    /**
+     * @param {Storage} storage
+     * @param {function(): Player} create
+     */
+
+    constructor(storage, create) {
+        this.#storage = storage;
+        this.#create = create;
+    }
+
+    /**
+     * @param {number} world
+     * @param {number} x
+     * @param {number} y
+     * @param {number} width
+     * @param {number} height
+     * @return {Player[]}
+     */
+
+    search(world, x, y, width, height) {
+        return [];
+    }
 }
