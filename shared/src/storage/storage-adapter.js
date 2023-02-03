c9d595d16054b6695259e70ebab42da1a9a899f1
RDiGregorio <robertdigreg@gmail.com>
2023-02-02 21:28:01 -0500

    experiments

--- shared/src/storage/storage-adapter.js
+++ shared/src/storage/storage-adapter.js
@@ -1,75 +1,17 @@
-import {Queue} from '../async/queue.js';
-
-// TODO: simplify this
-
-export const mockDatabase = new Map();
-
 /**
- * @param {string} location
- * @param {string} key
- * @return {Promise<boolean>}
+ * An abstract storage adapter.
  */
 
-export async function mockExists(location, key) {
-    return mockDatabase.get(location)?.has(key) ?? false;
-}
-
-/**
- * @param {string} location
- * @param {string} key
- * @return {Promise<*>}
- */
-
-export async function mockLoad(location, key) {
-    return mockDatabase.get(location)?.get(key);
-}
-
-/**
- * @param {string} location
- * @param {string} key
- * @param {*} value
- * @return {Promise<void>}
- */
-
-export async function mockSave(location, key, value) {
-    if (!mockDatabase.has(location)) mockDatabase.set(location, new Map());
-    mockDatabase.get(location).set(key, value);
-}
-
-export class StorageAdapter {
-    #queue = new Queue();
-    #location;
-    #exists;
-    #load;
-    #save;
-    #reviver;
-    #replacer;
-
-    /**
-     * @param {string} location
-     * @param {function(string, string): Promise<boolean>} exists
-     * @param {function(string, string): Promise<*>} load
-     * @param {function(string, string, *): Promise<void>} save
-     * @param {function(string, *): *} reviver
-     * @param {function(string, *): *} replacer
-     */
-
-    constructor(location, exists, load, save, reviver, replacer) {
-        this.#location = location;
-        this.#exists = exists;
-        this.#load = load;
-        this.#save = save;
-        this.#reviver = reviver;
-        this.#replacer = replacer;
-    }
+class StorageAdapter {
+    #storage = new Map();
 
     /**
      * @param {string} key
      * @return {Promise<boolean>}
      */
 
-    exists(key) {
-        return this.#queue.add(() => this.#exists(this.#location, key));
+    async exists(key) {
+        return this.#storage.has(key);
     }
 
     /**
@@ -77,8 +19,8 @@ export class StorageAdapter {
      * @return {Promise<*>}
      */
 
-    load(key) {
-        return this.#queue.add(async () => JSON.parse(await this.#load(this.#location, key), this.#reviver));
+    async load(key) {
+        return this.#storage.get(key);
     }
 
     /**
@@ -87,7 +29,7 @@ export class StorageAdapter {
      * @return {Promise<void>}
      */
 
-    save(key, value) {
-        return this.#queue.add(() => this.#save(this.#location, key, JSON.stringify(value, this.#replacer)));
+    async save(key, value) {
+        this.#storage.set(key, value);
     }
 }
