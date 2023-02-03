ea5678a28d2b46202e423cb769ee9e992228e067
RDiGregorio <robertdigreg@gmail.com>
2023-02-02 19:07:58 -0500

    experiments

--- shared/src/util/async.js
+++ shared/src/util/async.js
@@ -1,3 +1,9 @@
-export async function sleep(delay) {
-    await new Promise(resolve => setTimeout(resolve, delay));
+/**
+ * Returns a promise that resolves after `milliseconds`.
+ * @param {number} milliseconds
+ * @return {Promise<void>}
+ */
+
+export async function sleep(milliseconds) {
+    await new Promise(resolve => setTimeout(resolve, milliseconds));
 }
