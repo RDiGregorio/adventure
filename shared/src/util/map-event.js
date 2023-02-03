18f4fcc5fee51d2165c4b214caed5419256802f8
RDiGregorio <robertdigreg@gmail.com>
2023-02-02 19:09:32 -0500

    experiments

--- shared/src/util/map-event.js
+++ shared/src/util/map-event.js
@@ -1,5 +1,5 @@
 /**
- * An event for a `GameObject`.
+ * An event for an `EventfulMap`.
  */
 
 export class MapEvent {
@@ -20,7 +20,7 @@ export class MapEvent {
     }
 
     /**
-     * Returns a path to the event source.
+     * Returns the path to the event source.
      * @return {string[]}
      */
 
@@ -44,40 +44,5 @@ export class MapEvent {
 
     get value() {
         return this.#value;
-    }
-
-    /**
-     * A replacer for ending a `GameEvent` with `JSON.stringify`.
-     * @param {string} key
-     * @param {*} value
-     * @return {*}
-     */
-
-    static jsonReplacer(key, value) {
-        return value instanceof GameEvent
-            ? {class: value.constructor.name, type: value.type, path: value.path, value: value.value}
-            : value;
-    }
-
-    /**
-     * A reviver for decoding a `GameEvent` with `JSON.parse`.
-     * @param {string} key
-     * @param {*} value
-     * @return {*}
-     */
-
-    static jsonReviver(key, value) {
-        return value?.class === 'GameEvent' ? new GameEvent(value.type, value.path, value.value) : value;
-    }
-
-    /**
-     * TODO: move this somewhere else
-     * @param {string[]} array
-     * @return {boolean}
-     */
-
-    pathMatches(array) {
-        if (this.#path.length !== array.length) return false;
-        return this.#path.every((value, index) => array[index] === value || array[index] === '*');
     }
 }
