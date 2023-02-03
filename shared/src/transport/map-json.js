833ffff7e2168cf16a001ec459ca08edde1166d3
RDiGregorio <robertdigreg@gmail.com>
2023-02-03 18:33:39 -0500

    experiments

--- shared/src/transport/map-json.js
+++ shared/src/transport/map-json.js
@@ -1,5 +1,31 @@
 import {registeredClass} from '../util/reflect.js';
 
+class MapUtil {
+    /**
+     * Copies each entries from `source` to `target`.
+     * @param {Map} target
+     * @param {Map} source
+     */
+
+    static assign(target, source) {
+        [...source].forEach(entry => target.set(...entry));
+    }
+
+    /**
+     * Replaces the entries of `target` with the entries of `source`.
+     * @param {Map} target
+     * @param {Map} source
+     */
+
+    static sync(target, source) {
+        [...target.keys()]
+            .filter(key => !source.has(key) || source.get(key) !== target.get(key))
+            .forEach(key => target.delete(key));
+
+        MapUtil.assign(target, source);
+    }
+}
+
 /**
  * A map replacer for `JSON.stringify`.
  * @param {string} key
