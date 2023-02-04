e855949eb38c8b4564cd127b31d5d2647dbd14e9
RDiGregorio <robertdigreg@gmail.com>
2023-02-04 15:03:35 -0500

    experiments

--- shared/src/framework/game.js
+++ shared/src/framework/game.js
@@ -1,3 +1,28 @@
-export class Game {
+import {Model} from './model.js';
+import {EntitySpace} from '../entity/entity-space.js';
+import {View} from './view.js';
+import {Controller} from './controller.js';
 
+export class Game {
+    #controllers = new Map();
+    #views = new Map();
+    #model = new Model(new EntitySpace(), new EntitySpace(), new EntitySpace());
+
+    constructor() {
+        // TODO: add a game loop
+    }
+
+    get model() {
+        return this.#model;
+    }
+
+    view(id) {
+        if (!this.#views.has(id)) this.#views.set(id, new View(this.#model));
+        return this.#views.get(id);
+    }
+
+    controller(id) {
+        if (!this.#controllers.has(id)) this.#controllers.set(id, new Controller(null, null));
+        return this.#controllers.get(id);
+    }
 }
