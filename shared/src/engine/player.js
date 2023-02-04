beaee438ae9470daa8efd46eb2c3c7d550f730f0
RDiGregorio <robertdigreg@gmail.com>
2023-02-03 21:37:53 -0500

    experiments

--- shared/src/engine/player.js
+++ shared/src/engine/player.js
@@ -1,14 +1,1 @@
-import {GameObject} from './game-object.js';
-
-export class Player extends GameObject {
-    #entity;
-
-    constructor(entity) {
-        super();
-        this.#entity = entity;
-    }
-
-    get entity() {
-        return this.#entity;
-    }
-}
+// should this be called an 'account'?
