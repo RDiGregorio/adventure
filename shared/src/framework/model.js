0607f943282da9cbe27df28961ecfaf5da53188c
RDiGregorio <robertdigreg@gmail.com>
2023-02-04 14:31:35 -0500

    experiments

--- shared/src/framework/model.js
+++ shared/src/framework/model.js
@@ -1,2 +1,42 @@
+/**
+ * The entity spaces used for players, chunks, and pets.
+ */
+
 export class Model {
+    #playerSpace;
+    #chunkSpace;
+    #petSpace;
+
+    constructor(playerSpace, chunkSpace, petSpace) {
+        this.#playerSpace = playerSpace;
+        this.#chunkSpace = chunkSpace;
+        this.#petSpace = petSpace;
+    }
+
+    /**
+     * Returns the entity space for players.
+     * @return {EntitySpace}
+     */
+
+    get playerSpace() {
+        return this.#playerSpace;
+    }
+
+    /**
+     * Returns the entity space for chunks.
+     * @return {EntitySpace}
+     */
+
+    get chunkSpace() {
+        return this.#chunkSpace;
+    }
+
+    /**
+     * Returns the entity space for pets.
+     * @return {EntitySpace}
+     */
+
+    get petSpace() {
+        return this.#petSpace;
+    }
 }
