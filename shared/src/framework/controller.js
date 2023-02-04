f1bac8c33efd0c63aa368e5c4e2e80c973a1490a
RDiGregorio <robertdigreg@gmail.com>
2023-02-04 14:47:02 -0500

    experiments

--- shared/src/framework/controller.js
+++ shared/src/framework/controller.js
@@ -1,8 +1,9 @@
 // todo: this will handle the player manager and chunk manager
 // should each player have a different controller?
+// shoul this be like a session in TCO?
 
 /**
- * A game controller.
+ * A game controller. Each player in a game should have a different game controller.
  */
 
 export class Controller {
