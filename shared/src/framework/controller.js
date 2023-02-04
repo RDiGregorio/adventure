8ebdb5eee99ae51013c2bf0a3476884bcd5b87b4
RDiGregorio <robertdigreg@gmail.com>
2023-02-04 14:34:56 -0500

    experiments

--- shared/src/framework/controller.js
+++ shared/src/framework/controller.js
@@ -1,2 +1,11 @@
+// todo: this will handle the player manager and chunk manager
+
 export class Controller {
+    #playerManager;
+    #chunkManager;
+
+    constructor(playerManager, chunkManager) {
+        this.#playerManager = playerManager;
+        this.#chunkManager = chunkManager;
+    }
 }
