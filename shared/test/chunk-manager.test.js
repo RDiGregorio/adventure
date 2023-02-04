07029a1da99654c926cc47b3f8a3b08779967ed7
RDiGregorio <robertdigreg@gmail.com>
2023-02-04 00:21:24 -0500

    experiments

--- shared/test/chunk-manager.test.js
+++ shared/test/chunk-manager.test.js
@@ -9,20 +9,16 @@ const chunkSize = 100, saveMilliseconds = 1000;
 function create(world, x, y) {
     const result = [];
 
-    for (let i = 0; i < 1; i++)
+    for (let i = 0; i < 10; i++)
         result.push(new Entity('', '', x + i * 10, y + i * 10));
-
-    //console.log(result[0].get('location'));
 
     return result;
 }
 
-test('chunkManager.foo', async () => {
+test('chunkManager.search', async () => {
     const
         chunkManager = new ChunkManager(new EntitySpace(), new StorageAdapter(), chunkSize, saveMilliseconds, create),
         entities = await chunkManager.search('', 0, 0);
 
-    entities.forEach(entity => console.log([entity.x, entity.y]));
-//failed, 88?
-    expect(entities.length).toBe(9);
+    expect(entities.length).toBe(90);
 });
